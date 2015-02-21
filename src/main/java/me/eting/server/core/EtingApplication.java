68e6f31d408672ae8164f3c901f09207f1bdfdcd
lifenjoy51 <lifenjoy51@gmail.com>
2015-02-21 22:41:09 +0900

    init

---
+++ src/main/java/me/eting/server/core/EtingApplication.java
@@ -0,0 +1,27 @@
+package me.eting.server.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+/**
+ * Created by lifenjoy51 on 12/3/14.
+ */
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+public class EtingApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(EtingApplication.class);
+    }
+
+    @Bean
+    public StandardPasswordEncoder standardPasswordEncoder(){
+        return new StandardPasswordEncoder("eting");
+    }
+
+}
